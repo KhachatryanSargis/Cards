4ca77ef3bbda85700520e982ce402f7eab29f30d
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-08-23 17:52:39 +0400

    initial commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Sargis Khachatryan on 23.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
