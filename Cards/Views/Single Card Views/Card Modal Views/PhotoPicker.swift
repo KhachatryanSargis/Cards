76999393c615481addba56a5f974e73da0e2881e
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-08-26 16:11:59 +0400

    added photo picker and image drop functionality

---
+++ Cards/Views/Single Card Views/Card Modal Views/PhotoPicker.swift
@@ -0,0 +1,67 @@
+//
+//  PhotoPicker.swift
+//  Cards
+//
+//  Created by Sargis Khachatryan on 26.08.24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PhotoPicker: UIViewControllerRepresentable {
+    @Environment(\.dismiss) var dismiss
+    
+    class PhotosCoordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: PhotoPicker
+        
+        init(parent: PhotoPicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            let itemProviders = results.map(\.itemProvider)
+            for item in itemProviders {
+                if item.canLoadObject(ofClass: UIImage.self) {
+                    item.loadObject(ofClass: UIImage.self) { image, error in
+                        if let error = error {
+                            print("Error!", error.localizedDescription)
+                        } else {
+                            DispatchQueue.main.async {
+                                if let image = image as? UIImage {
+                                    self.parent.images.append(image)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            parent.dismiss()
+        }
+    }
+    
+    @Binding var images: [UIImage]
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 0
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> PhotosCoordinator {
+        PhotosCoordinator(parent: self)
+    }
+}
+
+#Preview {
+    PhotoPicker(images: .constant([]))
+}
--- Cards/Views/Single Card Views/CardDetailView.swift
+++ Cards/Views/Single Card Views/CardDetailView.swift
@@ -11,10 +11,12 @@ struct CardDetailView: View {
     @EnvironmentObject var viewState: ViewState
     @State private var currentModal: CardModal?
     @State private var stickerImage: UIImage?
+    @State private var images: [UIImage] = []
     @Binding var card: Card
         
     var body: some View {
         content
+            .onDrop(of: [.image], delegate: CardDrop(card: $card))
             .modifier(CardToolBar(currentModal: $currentModal))
             .sheet(item: $currentModal) { item in
                 switch item {
@@ -25,6 +27,14 @@ struct CardDetailView: View {
                                 card.addElement(uiImage: stickerImage)
                             }
                             stickerImage = nil
+                        }
+                case .photoPicker:
+                    PhotoPicker(images: $images)
+                        .onDisappear {
+                            for image in images {
+                                card.addElement(uiImage: image)
+                            }
+                            images = []
                         }
                 default:
                     EmptyView()
